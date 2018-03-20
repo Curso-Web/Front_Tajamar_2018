ef122d1da58e9383d3c0fc15ac46bda5cda4292c
alce65 <alce65@hotmail.es>
2018-03-20 21:31:06 +0100

    Objeto Math

---
+++ JS/05_APIS/math.js
@@ -0,0 +1,11 @@
+// x.algo es una INSTANCIA de String
+let x = "Pepe"
+x = x.toUpperCase()
+
+// aDatos es una INSTANCIA de Array
+let aDatos = [1, 12, 67]
+
+// Math NO SE INSTANCIA
+parseInt(34.99)
+Math.round(34.09)
+parseInt(Math.random()*10)
